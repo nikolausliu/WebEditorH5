de8b44ed1ba81be6c838598a4be442cee4731546
NikolausLiu <18010855044@163.com>
2020-03-14 15:02:04 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,8 @@ const autoprefixer = require('autoprefixer')
 const pxtoviewport = require('postcss-px-to-viewport')
 
 module.exports = {
-  outputDir: 'docs',
-  publicPath: process.env.NODE_ENV === 'production' ? '/vant-demo/' : '/',
+  outputDir: 'dist',
+  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
   css: {
     loaderOptions: {
       postcss: {

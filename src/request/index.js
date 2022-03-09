c09d07d9ceb039d46a4e4f39d0414653dc54144f
nikolausliu <18010855044@163.com>
2022-03-09 11:59:51 +0800

    feat: 修改接口地址

--- src/request/index.js
+++ src/request/index.js
@@ -3,7 +3,8 @@ import axios from 'axios'
 
 const isDev = process.env.NODE_ENV !== 'production'
 const localUrl = 'http://192.168.31.116:3000'
-const aliyunUrl = 'http://47.102.149.181:3001'
+// const aliyunUrl = 'http://47.102.149.181:3001'
+const aliyunUrl = 'https://nikolausliu.top/service-editor'
 axios.defaults.baseURL = isDev ? aliyunUrl : aliyunUrl
 
 axios.interceptors.request.use(
